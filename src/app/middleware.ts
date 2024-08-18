d4b7d13514eda909de91ed72db326e8f81c7f874
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2024-08-18 16:43:41 +0600

    sw: seccess

---
+++ src/app/middleware.ts
@@ -0,0 +1,17 @@
+import express, { urlencoded } from "express";
+import cookieParser from "express";
+import morgan from "morgan";
+import cors from "cors";
+import { corsOrigin } from "../config";
+
+const middleware = [
+  morgan("dev"),
+  cors({
+    origin: corsOrigin,
+  }),
+  cookieParser(),
+  // express.static("docs"),
+  express.json({ limit: "50mb" }),
+  urlencoded({ extended: true }),
+];
+export default middleware;
---
+++ src/routes/index.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+// import newsRoutes from "../modules/news/news.routes";
+
+type IModulerRoutes = { path: string; route: Router }[];
+
+export const modulerRoutes: IModulerRoutes = [
+  // {
+  //   path: "/",
+  //   route: newsRoutes,
+  // },
+];
